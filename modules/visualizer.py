a9b32f708ebf3839fa2a8c017971fd7c4b705ae1
omejiariv <145706652+omejiariv@users.noreply.github.com>
2025-10-21 22:54:09 -0500

    Update visualizer.py

--- modules/visualizer.py
+++ modules/visualizer.py
@@ -4352,11 +4352,11 @@ def display_life_zones_tab(**kwargs):
 
             # Llamar a la función de cálculo pasando resolución y máscara
             classified_raster, output_profile, name_map = generate_life_zone_map(
-                dem_path,
-                precip_raster_path,
+                dem_path, 
+                precip_raster_path, 
                 mean_latitude,
-                mask_geometry=mask_arg, # Pasar la geometría de la cuenca o None
-                downscale_factor=downscale_factor # Pasar el factor elegido
+                # mask_geometry=mask_arg, # <-- LÍNEA ELIMINADA O COMENTADA
+                downscale_factor=downscale_factor 
             )
 
             # Limpiar DEM temporal (AHORA se hace aquí, después de la llamada)
@@ -4459,6 +4459,7 @@ def display_life_zones_tab(**kwargs):
         
     elif not dem_path and os.path.exists(precip_raster_path):
          st.info("Sube un archivo DEM para habilitar la generación del mapa.")
+
 
 
 
