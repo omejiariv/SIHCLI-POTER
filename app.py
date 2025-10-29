a06628478ead3d39894a6afeafa409c681d85c36
omejiariv <145706652+omejiariv@users.noreply.github.com>
2025-10-29 16:12:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -273,7 +273,9 @@ def main():
         Config.MUNICIPALITY_COL, Config.ALTITUDE_COL, Config.REGION_COL, 
         Config.CELL_COL, Config.LATITUDE_COL, Config.LONGITUDE_COL, 
         Config.ET_COL,
-        Config.ENSO_ONI_COL
+        Config.ENSO_ONI_COL,
+        Config.SOI_COL,
+        Config.IOD_COL
         # Añade otras columnas si son necesarias (ENSO, etc.)
     ]
     existing_cols_to_keep = [col for col in cols_to_keep if col in df_monthly_filtered_intermediate.columns]
@@ -636,6 +638,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 
